54c6cc0f3148a7cecce1b71c6ba839526b0a36b7
elseptiawan <septiawanelshandi@gmail.com>
2024-07-14 01:23:29 +0700

    create suthorization middleware

---
+++ middlewares/authorization.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+const {sendResponse} = require('../helpers/response');
+
+authorization = (role) => {
+    return function (req, res, next) {
+        const authHeader = req.headers['authorization'];
+        const token = authHeader && authHeader.split(' ')[1];
+        if(token == null) {
+            return sendResponse(res, 401, 'Unauthorized');   
+        }
+        jwt.verify(token, process.env.API_SECRET, (err, decoded) => {
+            if(err) {
+                return sendResponse(res, 403, 'Forbidden');
+            }
+            if(role !== decoded.role) {
+                return sendResponse(res, 403, 'You Dont Have Permission');
+            }
+            req.userId = decoded.userId;
+            req.email = decoded.email;
+            req.role = decoded.role;
+            next();
+        });
+    }
+}
+
+module.exports = authorization;

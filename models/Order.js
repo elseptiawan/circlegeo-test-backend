1abf7edfa29235ccb8274ad62893a26ab4360bf4
elseptiawan <septiawanelshandi@gmail.com>
2024-07-14 02:56:39 +0700

    create order schema

---
+++ models/Order.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const Store = require('./Store');
+const Product = require('./Product');
+const User = require('./User');
+
+const order = mongoose.Schema({
+    store_id:{
+        required:true,
+        type:mongoose.Types.ObjectId,
+        ref: 'Store'
+    },
+    product_id:{
+        required:true,
+        type:mongoose.Types.ObjectId,
+        ref: 'Product'
+    },
+    user_id:{
+        required:true,
+        type:mongoose.Types.ObjectId,
+        ref: 'User'
+    },
+    totalPrice:{
+        required:true,
+        type:Number
+    },
+    qty:{
+        required:true,
+        type:Number
+    },
+    status:{
+        required:true,
+        type:Number,
+        enum: [0,1,2],
+        default: 0
+    },
+},
+{
+    timestamps:true
+});
+
+module.exports = mongoose.model("Order", order);
